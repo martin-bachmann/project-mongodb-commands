4486025de6cb771e8621ca106d7360db9774258b
Martin Bachmann <martinpezbach@gmail.com>
2022-11-09 16:10:00 -0300

    Requisito 14

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,10 @@
-db.produtos.find({ valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 } } } }, { nome: 1, _id: 0});
+db.produtos.find(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "proteínas", percentual: { $gte: 30, $lte: 40 },
+      },
+    },
+  },
+  { nome: 1, _id: 0 },
+);
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,4 @@
-db.produtos.find({ valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } } }, { nome: 1, _id: 0});
+db.produtos.find(
+  { valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } } },
+  { nome: 1, _id: 0 },
+);
